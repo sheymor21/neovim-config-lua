101b5579724cacf90ece9eb3ab203894e90094ff
sheymor21 <joseacvz81@gmail.com>
2025-12-14 14:52:55 -0400

    added harpoon2 with telescope support

---
+++ lua/plugins-keymaps/harpoon2-keymaps.lua
@@ -0,0 +1,32 @@
+local opts = { noremap = true, silent = true }
+local map = vim.keymap.set
+local ui = require("harpoon").ui
+
+-- Añadir archivo a Harpoon
+map("n", "<leader>a", function()
+	require("harpoon"):list():add()
+end, vim.tbl_extend("force", opts, { desc = "Harpoon: add file" }))
+
+-- Toggle quick menu de Harpoon
+map("n", "<leader>h", function()
+	ui:toggle_quick_menu(require("harpoon"):list())
+end, vim.tbl_extend("force", opts, { desc = "Harpoon: toggle menu" }))
+
+-- Navegar a marks 1-4
+map("n", "<leader>(", function()
+	require("harpoon"):list():select(1)
+end, opts)
+map("n", "<leader>)", function()
+	require("harpoon"):list():select(2)
+end, opts)
+map("n", "<leader>{", function()
+	require("harpoon"):list():select(3)
+end, opts)
+map("n", "<leader>}", function()
+	require("harpoon"):list():select(4)
+end, opts)
+
+-- Telescope nativo para marks
+map("n", "<leader>F", function()
+	require("telescope").extensions.harpoon.marks()
+end, vim.tbl_extend("force", opts, { desc = "Harpoon: Telescope marks" }))
---
+++ lua/plugins/harpoon2.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			local harpoon = require("harpoon")
+
+			harpoon:setup({
+				settings = {
+					save_on_toggle = true,
+					sync_on_ui_close = true,
+				},
+			})
+			require("../plugins-keymaps/harpoon2-keymaps")
+		end,
+	},
+}
