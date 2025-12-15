21197aa994e6c4e778aeec3ff20dd990b9623730
sheymor21 <joseacvz81@gmail.com>
2025-12-15 09:29:32 -0400

    remove lspconfig from vtsls

--- lua/lsp/vtsls.lua
+++ lua/lsp/vtsls.lua
@@ -1,44 +1,59 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("lspconfig").vtsls.setup({
-	capabilities = capabilities,
-	filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-
-	settings = {
-		vtsls = {
-			autoUseWorkspaceTsdk = true,
-		},
-
-		typescript = {
-			format = {
-				semicolons = "insert",
-				tabSize = 2,
-			},
-			preferences = {
-				importModuleSpecifier = "relative",
-			},
-		},
-
-		javascript = {
-			format = {
-				semicolons = "insert",
-				tabSize = 2,
-			},
-		},
-	},
-
-	root_dir = require("lspconfig.util").root_pattern("package.json", "tsconfig.json", ".git"),
-})
+local vtsls_cmd = {
+	vim.fn.stdpath("data") .. "/mason/bin/vtsls",
+	"--stdio",
+}
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+	pattern = {
+		"typescript",
+		"typescriptreact",
+		"javascript",
+		"javascriptreact",
+	},
 	callback = function(args)
-		local clients = vim.lsp.get_clients({ bufnr = args.buf })
-		for _, c in ipairs(clients) do
-			if c.name == "vtsls" then
+		-- Evita duplicar el cliente
+		for _, client in ipairs(vim.lsp.get_clients({ bufnr = args.buf })) do
+			if client.name == "vtsls" then
 				return
 			end
 		end
-		vim.lsp.start({ name = "vtsls" })
+
+		vim.lsp.start({
+			name = "vtsls",
+			cmd = vtsls_cmd,
+
+			capabilities = capabilities,
+
+			root_dir = vim.fs.root(args.buf, {
+				"package.json",
+				"tsconfig.json",
+				".git",
+			}),
+
+			settings = {
+				vtsls = {
+					autoUseWorkspaceTsdk = true,
+				},
+
+				typescript = {
+					format = {
+						semicolons = "insert",
+						tabSize = 2,
+					},
+					preferences = {
+						importModuleSpecifier = "relative",
+					},
+				},
+
+				javascript = {
+					format = {
+						semicolons = "insert",
+						tabSize = 2,
+					},
+				},
+			},
+		})
 	end,
 })
