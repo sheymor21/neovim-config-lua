86168e078ef3408fafe712400ca31b0812684a0b
sheymor21 <joseacvz81@gmail.com>
2025-12-24 01:30:04 -0400

    removed comment

--- lua/general-config.lua
+++ lua/general-config.lua
@@ -20,14 +20,33 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 vim.diagnostic.config({
 	virtual_text = {
-		prefix = "●", -- o "▎", "■", "", etc
+		prefix = "●",
 		spacing = 2,
 		severity = {
-			min = vim.diagnostic.severity.ERROR, -- solo errores
+			min = vim.diagnostic.severity.ERROR,
 		},
 	},
 	signs = true,
 	underline = true,
-	update_in_insert = false, -- no molesta mientras escribes
+	update_in_insert = false,
 	severity_sort = true,
+})
+
+-- Re-attach Tree-sitter cuando Harpoon cambia buffers
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	callback = function(args)
+		local ft = vim.bo[args.buf].filetype
+		if ft ~= "" then
+			pcall(vim.treesitter.start, args.buf)
+		end
+	end,
+})
+
+-- Asegurar filetype correcto
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback = function(args)
+		if vim.bo[args.buf].filetype == "" then
+			vim.cmd("filetype detect")
+		end
+	end,
 })
