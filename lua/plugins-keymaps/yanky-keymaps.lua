b0af93525e6801d36dfba66cbba3c516da7a2dec
sheymor21 <joseacvz81@gmail.com>
2025-12-24 01:58:30 -0400

    change mapping letters for yank history

--- lua/plugins-keymaps/yanky-keymaps.lua
+++ lua/plugins-keymaps/yanky-keymaps.lua
@@ -1,6 +1,6 @@
 require("telescope").load_extension("yank_history")
 local map = vim.keymap.set
 
-map("n", "<leader>y", function()
+map("n", "<leader>yh", function()
 	require("telescope").extensions.yank_history.yank_history()
 end, { desc = "Yank history" })
