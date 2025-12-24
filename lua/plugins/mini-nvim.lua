df90e11be0ac7259d4f6f467b6a5f154bf5b3616
sheymor21 <joseacvz81@gmail.com>
2025-12-24 01:34:57 -0400

    disabled scrolling for mini

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -1,8 +1,11 @@
 return {
-  "echasnovski/mini.nvim",
-  version = false,
-  config = function()
-    require("mini.animate").setup()
-  end,
+    "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+        require("mini.animate").setup({
+            scroll = { enabled = false },
+            cursor = { enabled = true },
+        })
+    end,
 
 }
