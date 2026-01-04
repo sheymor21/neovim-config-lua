3133610a156035b70ca3110fdaba7edf05f30b7f
sheymor21 <joseacvz81@gmail.com>
2026-01-04 17:06:44 -0400

    modify flash to replace f to s

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,24 +1,39 @@
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
-
     opts = {
         modes = {
             search = { enabled = false },
-            char = { enabled = true, jump_labels = true },
+            char = { enabled = false },
         },
-
         label = {
             rainbow = { enabled = false },
             uppercase = true,
+            before = true, -- Label antes del match
+            after = false, -- No poner label después
         },
-
         highlight = {
             groups = {
                 label = "IncSearch",
-                match = "Search",
+                match = "Normal",
                 backdrop = "Comment",
             },
         },
     },
+    keys = {
+        {
+            "f",
+            mode = { "n", "x", "o" },
+            function() require("flash").jump() end,
+            desc = "Flash Jump"
+        },
+        {
+            "F",
+            mode = { "n", "x", "o" },
+            function() require("flash").treesitter() end,
+            desc = "Flash Treesitter"
+        },
+        { "s", mode = { "n", "x", "o" }, false },
+        { "S", mode = { "n", "x", "o" }, false },
+    }
 }
