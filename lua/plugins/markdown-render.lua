03b3ca7793f1f991bca51541264d77d992fa9db5
sheymor21 <joseacvz81@gmail.com>
2026-01-06 01:04:37 -0400

    added markdown preview

---
+++ lua/plugins/markdown-render.lua
@@ -0,0 +1,40 @@
+return {
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+        ft = { "markdown" },
+        config = function()
+            require('render-markdown').setup({})
+        end,
+    },
+    {
+        "toppair/peek.nvim",
+        build = "deno task --quiet build:fast",
+        ft = { "markdown" },
+        keys = {
+            {
+                "<leader>mp",
+                function()
+                    local peek = require("peek")
+                    if peek.is_open() then
+                        peek.close()
+                    else
+                        peek.open()
+                    end
+                end,
+                desc = "Markdown Preview",
+            },
+        },
+        config = function()
+            require("peek").setup({
+                auto_load = true,
+                close_on_bdelete = true,
+                syntax = true,
+                theme = 'dark',
+                update_on_change = true,
+                app = 'browser', -- Cambia a 'browser' en lugar de 'webview'
+                filetype = { 'markdown' },
+            })
+        end,
+    }
+}
