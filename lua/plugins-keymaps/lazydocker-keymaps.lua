1d6787991e68120c4d6e28aa429101e437e2af26
sheymor21 <joseacvz81@gmail.com>
2026-01-15 12:02:10 -0400

    changed lazydocker keymap , now the command is one letter shorter

--- lua/plugins-keymaps/lazydocker-keymaps.lua
+++ lua/plugins-keymaps/lazydocker-keymaps.lua
@@ -1,6 +1,6 @@
 local lazydocker = require("../config.lazy-docker")
 local map = vim.keymap.set
 
-map("n", "<leader>idd", lazydocker.toggle, {
+map("n", "<leader>id", lazydocker.toggle, {
 	desc = "LazyDocker",
 })
