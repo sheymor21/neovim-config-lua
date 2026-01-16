7540f92f0d99a6adb77024f54599e7f058ce9c68
sheymor21 <joseacvz81@gmail.com>
2026-01-16 09:06:53 -0400

    added js and go in ensure_installed in mason

--- lua/plugins/configurationless.lua
+++ lua/plugins/configurationless.lua
@@ -1,30 +1,33 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"netcoredbg",
-			},
-		},
-		config = function()
-			require("mason").setup()
-		end,
-	},
+    {
+        "mason-org/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "netcoredbg",
+                "js-debug-adapter",
+                "delve",
 
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "omnisharp", "marksman", "vtsls", "html", "cssls", "jsonls" },
-			})
-		end,
-	},
-	{
-		"antosha417/nvim-lsp-file-operations",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = true,
-	},
+            },
+        },
+        config = function()
+            require("mason").setup()
+        end,
+    },
+
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "omnisharp", "marksman", "vtsls", "html", "cssls", "jsonls" },
+            })
+        end,
+    },
+    {
+        "antosha417/nvim-lsp-file-operations",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = true,
+    },
 }
