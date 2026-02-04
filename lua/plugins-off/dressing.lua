63e806ad21a4c1eb0b10c8fbf91b458d2b94de58
sheymor21 <joseacvz81@gmail.com>
2026-02-04 15:35:15 -0400

    deprecate dressing.lua

---
+++ lua/plugins-off/dressing.lua
@@ -0,0 +1,35 @@
+return {
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+    opts = {
+        input = {
+            enabled = true,
+            default_prompt = "➤ ",
+            title_pos = "center",
+            relative = "editor",
+            border = "rounded",
+            insert_only = false,
+            start_in_insert = true,
+            win_options = {
+                winblend = 0,
+            },
+        },
+
+        select = {
+            enabled = true,
+            backend = { "telescope", "builtin", "nui" },
+            telescope = require("telescope.themes").get_cursor({
+                initial_mode = "insert",
+            }),
+            builtin = {
+                show_numbers = true,
+                border = "rounded",
+                relative = "editor",
+                win_options = {
+                    winblend = 0,
+                },
+            },
+        },
+    },
+}
+
