4457d334557617b5ef82182a74fdc06f59cb4d74
sheymor21 <joseacvz81@gmail.com>
2026-02-16 09:53:18 -0400

    remove duplicate mapping in toggleterm

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -3,7 +3,6 @@ return {
     version = "*",
     cmd = "ToggleTerm",
     keys = {
-        { "<leader>t", "<cmd>ToggleTerm<CR>", desc = "Toggle Terminal" },
         { "<leader>ig", function() require("config.lazygit").toggle() end, desc = "LazyGit" },
         { "<leader>id", function() require("config.lazy-docker").toggle() end, desc = "LazyDocker" },
     },
