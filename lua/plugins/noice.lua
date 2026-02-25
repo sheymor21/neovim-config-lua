e44e95571175603c8b7c34e09ca233e1898ccc38
sheymor21 <joseacvz81@gmail.com>
2026-02-25 09:01:44 -0400

    update noice setup

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,102 +1,110 @@
+local notify_setup = {
+    stages = "static",
+    timeout = 2500,
+    fps = 60,
+    background_colour = "#1e1e1e",
+}
+
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		{
-			"rcarriga/nvim-notify",
-			config = function()
-				require("notify").setup({
-					stages = "static",
-					timeout = 2500,
-					fps = 60,
-					background_colour = "#1e1e1e",
-				})
-				vim.notify = require("notify")
-			end,
-		},
-	},
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        {
+            "rcarriga/nvim-notify",
+            config = function()
+                require("notify").setup(notify_setup)
+                vim.notify = require("notify")
+            end,
+        },
+    },
 
-	opts = {
-		-- PERFOMANCE MODE
-		render = "minimal",
-		stages = "static",
+    opts = {
+        -- PERFOMANCE MODE
+        render = "minimal",
+        stages = "static",
 
-		messages = {
-			enabled = true,
-			view = "mini",
-			view_error = "mini",
-			view_warn = "mini",
-			view_history = "messages",
-		},
+        messages = {
+            enabled = true,
+            view = "mini",
+            view_error = "mini",
+            view_warn = "mini",
+            view_history = "messages",
+        },
 
-		lsp = {
-			progress = {
-				enabled = true,
-				view = "mini",
-			},
+        lsp = {
+            progress = {
+                enabled = true,
+                view = "mini",
+            },
 
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true,
-			},
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
+            },
 
-			hover = {
-				enabled = true,
+            hover = {
+                enabled = true,
                 silent = true,
                 opts = { focusable = true, border = "rounded" },
-			},
+            },
 
-			signature = {
-				enabled = true,
-			},
+            signature = {
+                enabled = true,
+            },
 
-			message = {
-				enabled = true,
-			},
-		},
+            message = {
+                enabled = true,
+            },
+        },
 
-		cmdline = {
-			enabled = true,
-			view = "cmdline_popup",
-		},
+        cmdline = {
+            enabled = true,
+            view = "cmdline_popup",
+        },
 
-		popupmenu = {
-			enabled = true,
-			backend = "nui",
-		},
+        popupmenu = {
+            enabled = true,
+            backend = "nui",
+        },
 
-		presets = {
-			bottom_search = true,
-			command_palette = true,
-			long_message_to_split = true,
-			inc_rename = false,
-			lsp_doc_border = true,
-		},
+        presets = {
+            bottom_search = true,
+            command_palette = true,
+            long_message_to_split = true,
+            inc_rename = false,
+            lsp_doc_border = true,
+        },
 
-		-- integracion con DAP
-		routes = {
-			{
-				filter = { event = "msg_show", find = "DAP" },
-				view = "mini",
-			},
-			{ filter = { event = "notify", find = "Invalid mapping for  e" }, opts = { skip = true } },
-			{ filter = { event = "notify", find = "Invalid mapping for  i" }, opts = { skip = true } },
-			{
-				filter = {
-					event = "msg_show",
-					find = "OmniSharp",
-				},
-				view = "notify",
-			},
-			{
-				filter = {
-					event = "msg_show",
-					find = "roslyn",
-				},
-				view = "notify",
-			},
-		},
-	},
+        -- integracion con DAP
+        routes = {
+            {
+                filter = { event = "msg_show", find = "DAP" },
+                view = "mini",
+            },
+            {
+                filter = { event = "notify", find = "Invalid mapping for  e" },
+                opts = { skip = true },
+            },
+            {
+                filter = { event = "notify", find = "Invalid mapping for  i" },
+                opts = { skip = true },
+            },
+            {
+                filter = {
+                    event = "msg_show",
+                    find = "roslyn",
+                },
+                view = "notify",
+            },
+            {
+                filter = {
+                    event = "notify",
+                    find = "^%[",
+                },
+                view = "notify",
+            },
+        },
+    },
 }
