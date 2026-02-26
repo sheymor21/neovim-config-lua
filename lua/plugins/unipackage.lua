fa992c13e3d765f2f79cfe073de4dc780359d054
sheymor21 <joseacvz81@gmail.com>
2026-02-26 08:57:53 -0400

    added version_selection config to unipackage

--- lua/plugins/unipackage.lua
+++ lua/plugins/unipackage.lua
@@ -7,6 +7,15 @@ return {
         require("unipackage").setup({
             fallback_to_any = false,
             search_batch_size = 15,
+            version_selection = {
+                enabled = true,
+                languages = {
+                    javascript = false,
+                    dotnet = true,
+                },
+                include_prerelease = false,
+                max_versions_shown = 20,
+            },
         })
     end,
 }
