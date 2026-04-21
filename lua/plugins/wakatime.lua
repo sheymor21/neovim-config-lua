95947b75926b5cc051436b6b301a6ec557245c26
sheymor21 <joseacvz81@gmail.com>
2026-04-21 11:39:30 -0400

    fix wakatime-cli

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -1,4 +1,7 @@
 return {
 	"wakatime/vim-wakatime",
 	lazy = false,
+	-- Pinned to v12.0.0 (commit before --human-line-changes was added)
+	-- for wakatime-cli v1.60.4 compatibility
+	commit = "cb7ba05",
 }
