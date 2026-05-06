4478b86e7df8827dee19fa58a9693541f2a53b05
sheymor21 <joseacvz81@gmail.com>
2026-05-06 10:19:28 -0400

    added live grep to a file and global

--- lua/plugins-keymaps/fzf-lua-keymaps.lua
+++ lua/plugins-keymaps/fzf-lua-keymaps.lua
@@ -3,4 +3,7 @@ local map = vim.keymap.set
 map("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Fzf files" })
 map("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Fzf buffers" })
 map("n", "<leader>ss", "<cmd>FzfLua lsp_document_symbols<cr>", { desc = "Fzf symbols" })
-map("n", "<leader>sg", "<cmd>FzfLua live_grep<cr>", { desc = "Fzf live grep" })
+map("n", "<leader>sgg", "<cmd>FzfLua live_grep<cr>", { desc = "Fzf live grep (global)" })
+map("n", "<leader>sgf", function()
+	require("fzf-lua").live_grep({ search_paths = { vim.fn.expand("%:p") } })
+end, { desc = "Fzf live grep (current file)" })
