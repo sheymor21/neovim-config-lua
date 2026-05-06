00b2db7faae646bc06dba86149dbe3b9ebf12584
sheymor21 <joseacvz81@gmail.com>
2026-05-06 10:21:55 -0400

    revome snack override from ui.select and ui.input

--- lua/config/snacks.lua
+++ lua/config/snacks.lua
@@ -3,12 +3,3 @@ local Snacks = require("snacks")
 
 -- Set vim.notify to use Snacks notifier
 vim.notify = Snacks.notifier.notify
-
--- Override vim.ui.select and vim.ui.input to use snacks
-vim.ui.select = function(items, opts, on_choice)
-	Snacks.picker.select(items, opts, on_choice)
-end
-
-vim.ui.input = function(opts, on_confirm)
-	Snacks.input(opts, on_confirm)
-end
