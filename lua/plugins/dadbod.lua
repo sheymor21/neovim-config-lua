79908ef27dfb3a83799c4eebd6286607050e8bc0
sheymor21 <josearmando_081@outlook.com>
2026-08-05 08:41:15 -0400

    db plugin now isnt lazy

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -1,21 +1,17 @@
 return {
     "tpope/vim-dadbod",
-    cmd = { "DB", "DBUIToggle", "DBUIRenameBuffer", "DBUIExecuteQuery", "DBUIToggleNERDTree", "DBUIAddConnection", "DBUIBackspace" },
-    ft = { "sql" },
-
+    lazy = false,
     dependencies = {
         {
             "kristijanhusak/vim-dadbod-ui",
-            cmd = { "DBUIToggle", "DBUICloseTab", "DBUIRenameBuffer", "DBUIExecuteQuery", "DBUIToggleNERDTree", "DBUIAddConnection" },
-            ft = { "sql" },
+            lazy = false,
             config = function()
                 require("config.dadbod")
             end,
         },
         {
             "kristijanhusak/vim-dadbod-completion",
-            cmd = { "DBCompletionClearCache" },
-            ft = { "sql", "mysql", "plsql" },
+            lazy = false,
             dependencies = { "nvim-lua/plenary.nvim" },
             -- No setup needed: blink.cmp integration is handled in lua/plugins/blink-cmp.lua
             -- via the providers table (module = "vim_dadbod_completion.blink").
