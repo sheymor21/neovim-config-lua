e418283af0cd4f762ee9ebff179a9c47d9b29d25
sheymor21 <josearmando_081@outlook.com>
2026-09-07 11:10:26 -0400

    added modicator

---
+++ lua/plugins/modicator.lua
@@ -0,0 +1,12 @@
+return {
+    "mawkler/modicator.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.cursorline = true
+        vim.o.number = true
+        vim.o.termguicolors = true
+    end,
+    opts = {
+        show_warnings = true,
+    },
+}
